576092dab6bd04c9a5fabd8434e91156c6cf1aa2
Mateus Cavalheiro <wateuz@hotmail.com>
2026-04-28 19:59:12 -0300

    basics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import models, database
-from routers import esp32
+from routers import esp32, streamlit
 
 # Cria as tabelas no banco de dados automaticamente
 models.Base.metadata.create_all(bind=database.engine)
@@ -9,6 +9,7 @@ app = FastAPI(title="API de Etiquetas Inteligentes")
 
 # Inclui as rotas
 app.include_router(esp32.router)
+app.include_router(streamlit.router)
 
 @app.get("/")
 def read_root():
